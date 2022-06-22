27470ac6b6d66e09d318a5c992304024068e5db5
bujarasllani <bujarasllani1@gmail.com>
2022-06-23 01:49:39 +0200

    Add files via upload

---
+++ src/Filler/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const Filler = () => {
+  return (
+    <p className="lorem-text">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla iure
+      voluptatibus, eligendi labore eveniet repudiandae, dolore, dolor modi
+      ipsam eum accusantium fuga atque placeat iste molestiae quisquam quidem
+      suscipit omnis. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Fugit sit et dolore officiis cum voluptatem reprehenderit aliquam
+      aspernatur iure similique quibusdam autem voluptatibus natus molestiae
+      dicta maxime sint, iste temporibus. Lorem
+
+      /nDieser Text wurde eingetragen nur zu Sehen wie der Kontakt Formular aussieht
+    </p>
+  );
+};
+
+export default Filler;
